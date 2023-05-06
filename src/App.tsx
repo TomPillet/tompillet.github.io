619b968e4f29acdbaa3717a6c65b899833893e6b
Tom <tom963741@gmail.com>
2023-05-06 17:47:38 +0200

    feat: setup and first layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <nav className='navbar'>
+          <h6 className="title">Tom Pillet</h6>
+          <div className="links">
+            <ul className='links-list'>
+              <li className="link-item">
+                <a id="github" href="">Github</a>
+              </li>
+              <li className="link-item">
+                <a id="linkedin" href="">Linkedin</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
     </div>
   );
