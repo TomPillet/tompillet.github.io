8ef63548359bae93689d39e6e05aada1645f5331
Tom <tom963741@gmail.com>
2023-05-07 10:19:30 +0200

    feat: animated neon under the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
-function App() {
+const App = () => {
+  const [neonOpacity, setNeonOpacity] = useState(100);
+  let randIntervalDelay = (neonOpacity != 100) ? 80 : Math.floor(Math.random() * (1000 - 200) + 200);
+
+  let randTrueOrFalse = () => {
+    return (Math.floor(Math.random() * (3 - 1) + 1) % 2) === 0;
+  }
+
+  useEffect(() => {
+    const neonInterval = setInterval(() => {
+      setNeonOpacity((randTrueOrFalse()) ? 100 : (Math.random() * neonOpacity));
+    }, randIntervalDelay);
+    
+    return () => clearInterval(neonInterval);
+  })
+
   return (
     <div className="App">
       <header className='header'>
@@ -11,14 +25,16 @@ function App() {
           <div className="links">
             <ul className='links-list'>
               <li className="link-item">
-                <a id="github" href="">Github</a>
+                <a id="github" target='blank' href="https://github.com/TomPillet">Github</a>
               </li>
               <li className="link-item">
-                <a id="linkedin" href="">Linkedin</a>
+                <a id="linkedin" target='blank' href="https://www.linkedin.com/in/tom-pillet/">Linkedin</a>
               </li>
             </ul>
           </div>
         </nav>
+
+        <span className="navbar-neon" style={{'opacity': neonOpacity+'%'}}></span>
       </header>
     </div>
   );
