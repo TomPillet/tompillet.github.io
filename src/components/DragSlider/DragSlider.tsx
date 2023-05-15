b4485f0721c480f8c7a19a8ae61530b97f7ea1ad
Tom <tom963741@gmail.com>
2023-05-16 01:39:00 +0200

    feat: add buttons on the slider and display them on condition

--- src/components/DragSlider/DragSlider.tsx
+++ src/components/DragSlider/DragSlider.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './DragSlider.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 interface DragSliderProps {
   id: string,
@@ -7,51 +9,80 @@ interface DragSliderProps {
 }
 
 export const DragSlider: FC<DragSliderProps> = ({id, data}) => {
-  let isDown = false;
-  
+  let sliderActive = false;
+
   const onClickDrag = (e: any) => {
     const slider = document.getElementById(id)!;
+    const sliderButtonLeft = document.querySelector('.drag-slider-button.left');
+    const sliderButtonRight = document.querySelector('.drag-slider-button.right');
+    sliderActive = true;
+    hideOrShowButtons(sliderButtonLeft, sliderButtonRight, slider);
+
     let startX = e.pageX - slider.offsetLeft;
     let scrollLeft = slider.scrollLeft;
     slider.classList.add('on-drag');
-    isDown = true;
     e.preventDefault();
 
-    slider.addEventListener('mousemove', (e) => {
-      if (!isDown) return;
+    slider.addEventListener('mousemove', (e: any) => {
+      if (!sliderActive) return;
       e.preventDefault();
       const scrollX = e.pageX - slider.offsetLeft;
       const walk = (scrollX - startX) * 3;
       slider.scrollLeft = scrollLeft - walk;
+
     });
     slider.addEventListener('mouseup', () => {
       removeDragStatement(slider);
+      hideOrShowButtons(sliderButtonLeft, sliderButtonRight, slider);
     });
     slider.addEventListener('mouseleave', () => {
       removeDragStatement(slider);
+      hideOrShowButtons(sliderButtonLeft, sliderButtonRight, slider);
     });
   }
 
   const removeDragStatement = (slider: any) => {
     slider.classList.remove('on-drag');
-    isDown = false;
+    sliderActive = false;
+  }
+
+  const hideOrShowButtons = (buttonLeft: any, buttonRight: any, slider: any) => {
+    if (slider.scrollLeft === 0 || sliderActive) {
+      buttonLeft.classList.add('hide');
+    } else {
+      buttonLeft.classList.remove('hide');
+    }
+    if (slider.scrollLeft === slider.scrollLeftMax || sliderActive) {
+      buttonRight.classList.add('hide');
+    } else {
+      buttonRight.classList.remove('hide');
+    }
   }
 
   return (
-    <div id={id} className='drag-slider' onMouseDown={onClickDrag}>
-      {
-        data.map(item => {
-          return (
-            <div key={item.id} className='item'>
-              <div 
-                className="item-image"
-                style={{backgroundImage: "url("+require('../../'+item.url)+")"}}
-              ></div>
-              <span className='item-title'>{item.title}</span>
-            </div>
-          )
-        })
-      }
+    <div className='drag-slider-container'>
+      <button className='drag-slider-button left hide'>
+        <FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon>
+      </button>
+      <button className='drag-slider-button right'>
+        <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+      </button>
+
+      <div id={id} className='drag-slider' onMouseDown={onClickDrag}>
+        {
+          data.map(item => {
+            return (
+              <div key={item.id} className='item'>
+                <div 
+                  className="item-image"
+                  style={{backgroundImage: "url("+require('../../'+item.url)+")"}}
+                ></div>
+                <span className='item-title'>{item.title}</span>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 };
