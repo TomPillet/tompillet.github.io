5e712f0432c1b540bf812ad00a86fc28fd87e2c9
Tom <tom963741@gmail.com>
2023-05-25 04:06:55 +0200

    feat: add languages dropdown to change current app language

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,25 @@
 import React, { FC } from 'react';
 import './AppHeader.scss';
+import i18next, { changeLanguage } from 'i18next';
+
+import iconEN from '../../medias/flags/en-icon.svg';
+import iconFR from '../../medias/flags/fr-icon.svg';
 
 interface AppHeaderProps {}
 
 const AppHeader: FC<AppHeaderProps> = () => (
   <header className='header'>
     <nav className='navbar'>
+      <div className="languages-dropdown">
+        <div className="current-language">
+          <img className="language-flag" src={(i18next.language === 'fr') ? iconFR : iconEN} alt="Current language" />
+        </div>
+
+        <div className="languages-list">
+          <button className="language-button" onClick={() => changeLanguage('en')}> <img className="language-flag" src={iconEN} alt="English flag" /> <span>EN</span> </button>
+          <button className="language-button" onClick={() => changeLanguage('fr')}> <img className="language-flag" src={iconFR} alt="French flag" /> <span>FR</span> </button>
+        </div>
+      </div>
       <h6 className="title">Tom Pillet</h6>
       <div className="links">
         <ul className='links-list'>
