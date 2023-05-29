fcf65eedebc1824ddc45398d4de9710f5d64fe41
Tom <tom963741@gmail.com>
2023-05-29 15:48:14 +0200

    feat: use return to render Card content

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -11,32 +11,34 @@ interface CardProps {
   cardData: any
 }
 
-const Card: FC<CardProps> = ({cardData}) => (
-  <Color src={require('../../'+cardData.background)} format='hex'>
-    {({ data, loading, error }) => (
-      <div id={cardData.id} className="card-inner">
-        <div className="card-header">
-          <div className='card-background'
-              style={{
-                backgroundImage: `url(${require('../../'+cardData.background)})`,
-                borderColor: data
-              }}>
+const Card: FC<CardProps> = ({cardData}) => {
+  return (
+    <Color src={require('../../'+cardData.background)} format='hex'>
+      {({ data, loading, error }) => (
+        <div id={cardData.id} className="card-inner">
+          <div className="card-header">
+            <div className='card-background'
+                style={{
+                  backgroundImage: `url(${require('../../'+cardData.background)})`,
+                  borderColor: data
+                }}>
+            </div>
+            <h5 className="card-title" style={{textShadow: `2px 2px 0px `+data}}>
+              <span className='dynamic-underline'>{cardData.title}</span>
+            </h5>
           </div>
-          <h5 className="card-title" style={{textShadow: `2px 2px 0px `+data}}>
-            <span className='dynamic-underline'>{cardData.title}</span>
-          </h5>
-        </div>
 
-        <div className="card-description">
-          {cardData.description}
-        </div>
+          <div className="card-description">
+            {cardData.description}
+          </div>
 
-        <a className='button action' href={cardData.linkto} target='blank'>
-          Voir le repo <FontAwesomeIcon icon={faGithub as IconProp}></FontAwesomeIcon>
-        </a>
-      </div>
-    )}
-  </Color>
-);
+          <a className='button action' href={cardData.linkto} target='blank'>
+            Voir le repo <FontAwesomeIcon icon={faGithub as IconProp}></FontAwesomeIcon>
+          </a>
+        </div>
+      )}
+    </Color>
+  )
+};
 
 export default Card;
