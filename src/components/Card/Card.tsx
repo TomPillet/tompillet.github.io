ab3249362aa78b891501492491a7c9c75278c003
Tom <tom963741@gmail.com>
2023-05-29 16:22:44 +0200

    feat: create a card-content element

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -16,21 +16,23 @@ const Card: FC<CardProps> = ({cardData}) => {
     <Color src={require('../../'+cardData.background)} format='hex'>
       {({ data, loading, error }) => (
         <div id={cardData.id} className="card-inner">
-          <div className="card-header">
-            <img className='card-background' src={require('../../'+cardData.background)}
-                alt={cardData.title} style={{borderColor: data}} />
-            <h5 className="card-title" style={{textShadow: `2px 2px 0px `+data}}>
-              <span className='dynamic-underline'>{cardData.title}</span>
-            </h5>
-          </div>
+          <div className="card-content">
+            <div className="card-header">
+              <img className='card-background' src={require('../../'+cardData.background)}
+                  alt={cardData.title} style={{borderColor: data}} />
+              <h5 className="card-title" style={{textShadow: `2px 2px 0px `+data}}>
+                <span className='dynamic-underline'>{cardData.title}</span>
+              </h5>
+            </div>
 
-          <div className="card-description">
-            {cardData.description}
-          </div>
+            <div className="card-description">
+              {cardData.description}
+            </div>
 
-          <a className='button action' href={cardData.linkto} target='blank'>
-            Voir le repo <FontAwesomeIcon icon={faGithub as IconProp}></FontAwesomeIcon>
-          </a>
+            <a className='button action' href={cardData.linkto} target='blank'>
+              Voir le repo <FontAwesomeIcon icon={faGithub as IconProp}></FontAwesomeIcon>
+            </a>
+          </div>
         </div>
       )}
     </Color>
