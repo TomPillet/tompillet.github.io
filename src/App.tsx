89c0793c0246746be002f52597f156a0fb145243
TomPillet <tom.pilletgaulon@gmail.com>
2024-05-23 16:26:28 +0200

    add birthday card + new colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import i18next from 'i18next';
 import { useTranslation } from 'react-i18next';
 
 import './App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload, faCakeCandles } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faCakeCandles, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 import AppHeader from './components/AppHeader/AppHeader';
 import DragSlider from './components/DragSlider/DragSlider';
@@ -19,6 +19,53 @@ const App = () => {
   let projectsList = require("./json/projects/"+i18next.language.substring(0,2)+"/projects.json");
   let projectsResources = require("./json/projects/projects-resources.json");
 
+  const year = new Date().getFullYear();
+  const birthday = new Date(year, 10, 13).getTime();
+  const today = Date.now();
+  let remainingTime = (birthday - today);
+
+  // const [birthdayMonthsLeft, setBirthdayMonthsLeft] = useState(0);
+
+  let [birthdayDaysLeft, setBirthdayDaysLeft] = useState(Math.floor(remainingTime/1000/60/60/24));
+  remainingTime = remainingTime-(birthdayDaysLeft*86400*1000);
+  let [birthdayHoursLeft, setBirthdayHoursLeft] = useState(Math.floor(remainingTime/1000/60/60));
+  remainingTime = remainingTime-(birthdayHoursLeft*3600*1000);
+  let [birthdayMinutesLeft, setBirthdayMinutesLeft] = useState(Math.floor(remainingTime/1000/60));
+  remainingTime = remainingTime-(birthdayMinutesLeft*60*1000);
+  let [birthdaySecondsLeft, setBirthdaySecondsLeft] = useState(Math.floor(remainingTime/1000));
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      console.log('ok');
+      if (birthdaySecondsLeft > 0) {
+        setBirthdaySecondsLeft(birthdaySecondsLeft - 1);
+        console.log(birthdaySecondsLeft);
+      } else if (birthdayMinutesLeft > 0) {
+        setBirthdayMinutesLeft(birthdayMinutesLeft - 1);
+        setBirthdaySecondsLeft(59);
+      } else if (birthdayHoursLeft > 0) {
+        setBirthdayHoursLeft(birthdayHoursLeft - 1);
+        setBirthdayMinutesLeft(59);
+        setBirthdaySecondsLeft(59);
+      } else if (birthdayDaysLeft > 0) {
+        setBirthdayDaysLeft(birthdayDaysLeft - 1);
+        setBirthdayHoursLeft(23);
+        setBirthdayMinutesLeft(59);
+        setBirthdaySecondsLeft(59);
+      } 
+      // else if (birthdayMonthsLeft > 0) {
+      //   setBirthdayMonthsLeft((birthdayMonthsLeft) => birthdayMonthsLeft--);
+      //   setBirthdayDaysLeft(30);
+      //   setBirthdayHoursLeft(23);
+      //   setBirthdayMinutesLeft(59);
+      //   setBirthdaySecondsLeft(59);
+      // }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [birthdayDaysLeft, birthdayHoursLeft, birthdayMinutesLeft, birthdaySecondsLeft]);
+
+
   return (
     <div className="App">
       <AppHeader></AppHeader>
@@ -27,18 +74,20 @@ const App = () => {
         <div className="wrapper presentation-wrapper">
           <div className='wrapper-content'>
             <div className='photo-cv-container'>
-              <div className="anniversary-trigger">
+              <div className="birthday-trigger">
                 <span><FontAwesomeIcon className='icon' icon={faCakeCandles}/></span>
               </div>
-              <div className='anniversary-text'>
-                <p>Mon anniversaire est le 13 novembre de chaque année. J'ai aujourd'hui 22 ans.</p>
+              <div className='birthday-text'>
+                <p>Prochain anniversaire dans <b>{birthdayDaysLeft}</b> jours, <b>{birthdayHoursLeft}</b> heures, <b>{birthdayMinutesLeft}</b> minutes et <b>{birthdaySecondsLeft}</b> secondes.</p>
               </div>
               <img id="photoCV" src={photoCV} alt="Tom PILLET"/>
             </div>
 
             <div className='presentation-container'>
               <h1 className='presentation-title'>Tom PILLET</h1>
-              <p className="presentation-text">{t("presentation_text")}</p>
+              <p className="presentation-text underline">{t("presentation_text")}</p>
+              <p className="presentation-situation">{t("presentation_situation")}</p>
+              <span className="presentation-location"><FontAwesomeIcon className='icon' icon={faLocationDot}></FontAwesomeIcon> Besançon, France</span>
             </div>
 
             {/* <a id="downloadCV" className='button action' href={"CV_TomPILLET-"+i18next.language+".pdf"} download>
